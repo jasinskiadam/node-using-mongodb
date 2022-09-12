e7cd0b2958fd15c2448578601749af8c4f0ac6c4
jasinskiadam <admjasinski@gmail.com>
2022-09-12 12:42:09 +0200

    feat: add changing data with CRUD

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,15 +2,18 @@ import {
   addnewProduct,
   getProductWithID,
   getProducts,
+  updateProduct,
+  deleteProduct,
 } from '../controllers/controllers';
 
 const routes = (app) => {
-  app.route('/products')
+  app
+    .route('/products')
     .get(getProducts)
 
     //Post endpoint
     .post(addnewProduct);
-  app.route('/products/:ProductID').get(getProductWithID);
+  app.route('/products/:ProductID').get(getProductWithID).put(updateProduct).delete(deleteProduct);
 };
 
 export default routes;
